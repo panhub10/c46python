e06f1f684f3cabb67750c30e4caded0f5b66dd9f
panhub10 <253724363@qq.com>
2022-10-31 17:01:57 +0800

    cxy1提交代码

---
+++ pythonpro/day02/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/10/31 16:59
+Copyright (c) 2022/10/31, 海牛学院版权所有.
+@author: 潘牛
+"""
---
+++ pythonpro/day02/hello_world.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+hello_world.py
+Created on 2022/10/31 16:59
+Copyright (c) 2022/10/31, 海牛学院版权所有.
+@author: 潘牛
+"""
+print("hello world")
